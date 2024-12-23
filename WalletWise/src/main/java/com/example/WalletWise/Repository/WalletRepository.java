92d4a4c9c033e3256f14534d1641ead8e0c89021
Aleena-Memon <maleena624@gmail.com>
2024-12-23 11:44:34 +0500

    updated

--- WalletWise/src/main/java/com/example/WalletWise/Repository/WalletRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.WalletWise.Repository;
-
-import com.example.WalletWise.Entity.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet,String> {
-}
