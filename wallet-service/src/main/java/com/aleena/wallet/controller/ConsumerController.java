67c7f23d28994af6ddb1915045eabc0dffca12f1
Aleena-Memon <maleena624@gmail.com>
2024-12-25 15:14:04 +0500

    controller updated

--- wallet-service/src/main/java/com/aleena/wallet/controller/ConsumerController.java
+++ wallet-service/src/main/java/com/aleena/wallet/controller/ConsumerController.java
@@ -1,5 +1,7 @@
 package com.aleena.wallet.controller;
 
+import com.aleena.wallet.controller.request.ConsumerRequest;
+import com.aleena.wallet.controller.response.ConsumerResponse;
 import com.aleena.wallet.repository.entity.Consumer;
 import com.aleena.wallet.service.ConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/consumers")
@@ -16,16 +19,44 @@ public class ConsumerController {
     private ConsumerService consumerService;
 
     @GetMapping
-    public List<Consumer> getAllConsumers(){
-        return consumerService.getAllConsumers();
+    public List<ConsumerResponse> getAllConsumers() {
+        List<Consumer> consumers = consumerService.getAllConsumers();
+
+        List<ConsumerResponse> consumerResponses = consumers.stream()
+                .map(consumer -> new ConsumerResponse(
+                        consumer.getConsumerId(),
+                        consumer.getUamId(),
+                        consumer.getName(),
+                        consumer.getType(),
+                        consumer.getWallets().stream().map(wallet -> wallet.getWalletId())
+                                .collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
+
+        return consumerResponses;
     }
 
-    @PostMapping
-    public ResponseEntity<Consumer> createConsumer(@RequestBody Consumer consumer){
-     Consumer saveConsumer = consumerService.createConsumer(consumer);
-     return new ResponseEntity<Consumer>(saveConsumer, HttpStatus.CREATED);
+   @PostMapping
+    public ResponseEntity<ConsumerResponse> createConsumer(@RequestBody ConsumerRequest consumerRequest){
+        Consumer consumer = new Consumer(
+                "",// autoincrement
+                consumerRequest.getUamId(),
+                consumerRequest.getName(),
+                consumerRequest.getType(),
+                List.of()  // empty list
+        );
 
-    }
+        Consumer consumerSaved = consumerService.createConsumer(consumer);
 
-
+        // map this consumer to response
+       ConsumerResponse response = new ConsumerResponse(
+               consumerSaved.getConsumerId(),
+               consumerSaved.getUamId(),
+               consumerSaved.getName(),
+               consumerSaved.getType(),
+               consumerSaved.getWallets() != null ? consumerSaved.getWallets().stream()
+                       .map(wallet -> wallet.getWalletId()).collect(Collectors.toList()) : List.of()
+       );
+       return new ResponseEntity<>(response, HttpStatus.CREATED);
+   }
 }
--- wallet-service/src/main/java/com/aleena/wallet/controller/response/ConsumerResponse.java
+++ wallet-service/src/main/java/com/aleena/wallet/controller/response/ConsumerResponse.java
@@ -1,12 +1,14 @@
 package com.aleena.wallet.controller.response;
 
 import com.aleena.wallet.repository.entity.ConsumerType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 
 @Data
+@AllArgsConstructor
 public class ConsumerResponse {
     private String consumerId;
     private String uamId;
