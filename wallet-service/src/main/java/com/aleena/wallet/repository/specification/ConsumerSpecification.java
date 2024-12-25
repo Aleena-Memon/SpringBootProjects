bd3a461112e8d6d8dac6665eab4adccad5b4ab1a
Aleena-Memon <maleena624@gmail.com>
2024-12-25 21:08:00 +0500

    consumer Entity updated

---
+++ wallet-service/src/main/java/com/aleena/wallet/repository/specification/ConsumerSpecification.java
@@ -0,0 +1,26 @@
+package com.aleena.wallet.repository.specification;
+
+import com.aleena.wallet.repository.entity.Consumer;
+import com.aleena.wallet.repository.entity.PaymentMeans;
+import com.aleena.wallet.repository.entity.Wallet;
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ConsumerSpecification {
+
+    public static Specification<Consumer> findByWalletId(String walletId){
+        return(root, query,criteriaBuilder )->{
+            Join<Consumer, Wallet> walletJoin = root.join("walletId");
+            return criteriaBuilder.equal(walletJoin.get("walletId"), walletId);
+        };
+    }
+
+    public static Specification<Consumer> findByPaymentMeanId(String paymentMeanId){
+        return (root, query, criteriaBuilder)->{
+            Join<Consumer, Wallet> walletJoin = root.join("walletId");
+            Join<Wallet, PaymentMeans> paymentMeanJoin = walletJoin.join("paymentMeanId");
+            return criteriaBuilder.equal(paymentMeanJoin.get("paymentMeanId"),paymentMeanId);
+        };
+    }
+
+}
